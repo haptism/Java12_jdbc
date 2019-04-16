05914eac6de19d7304e6f5f69d83414d4144556c
{haptism} <haptism007@gmail.com>
2019-04-16 13:46:17 +0900

    ;
    l

--- src/com/rjm/db/InsertTest2.java
+++ src/com/rjm/db/InsertTest2.java
@@ -1,64 +1,19 @@
-//package com.rjm.db;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.Scanner;
-//
-//public class InsertTest2 {
-//
-//	public static void main(String[] args) {
-//
-//		// Scanner로 입력 받아서
-//		// 부서번호, 부서명, 지역명을 입력 받아서
-//		// dept 테이블에 insert
-//
-//		Scanner sc = new Scanner(System.in);
-//
-//		String user = "scott";
-//		String password = "tiger";
-//		String url = "jdbc:oracle:thin:@211.238.142.21:1521:xe"; // SID
-//		String driver = "oracle.jdbc.driver.OracleDriver";
-//		Connection con;
-//		Statement st;
-//		int result;
-//
-//		// 2. driver를 메모리에 로딩
-//
-//		try {
-//			Class.forName(driver);
-//			System.out.println("드라이버 로딩 성공");
-//
-//			// 3. DB서비스에 접속해서 로그인해서 Connection 받기
-//			con = DriverManager.getConnection(url, user, password);
-//			System.out.println("연결성공");
-//
-//			// 4. Query(SQL)문 생성 - 문자열 // ; 생략
-//			String sql = sc.next();
-//
-//			// 5. 전송
-//			st = con.createStatement();
-//			result = st.executeUpdate(sql);
-//
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//
-//		} finally {
-//			try {
-//				st.close();
-//				con.close();
-//			} catch (SQLException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		}
-//
-//		if (result > 0) {
-//			System.out.println("성공");
-//		} else {
-//			System.out.println("실패");
-//		}
-//	}
-//}
+package com.rjm.db;
+
+public class InsertTest2 {
+
+	public static void main(String[] args) {
+
+		// Scanner로 입력 받아서
+		// 부서번호, 부서명, 지역명을 입력 받아서
+		// dept 테이블에 insert
+
+		int deptno = 13;
+		String dname = "HR";
+		String loc = "Paris";
+
+		String sql = "insert into dept" + " values(" + deptno + ", ''" + dname + "','" + loc + "')";
+		System.out.println(sql);
+
+	}
+}
--- src/com/rjm/db/InsertTest3.java
+++ src/com/rjm/db/InsertTest3.java
@@ -1,62 +1,14 @@
 package com.rjm.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
-
 public class InsertTest3 {
 
 	public static void main(String[] args) {
-
-		// 1. DB연결 로그인
-
-		Scanner sc = new Scanner(System.in);
 		
-		String user = "scott";
-		String password = "tiger";
-		String url = "jdbc:oracle:thin:@211.238.142.21:1521:xe"; // SID
-		String driver = "oracle.jdbc.driver.OracleDriver";
-		Connection con = null;
-		Statement st = null;
-		int result = 0;
+		// EMP 테이블에 insert
+		// 사번 중복되면 에러
+		// 8가지 입력 받기 DEPTNO = 30(통일), COMM = 0, SAL = 1000, HIREDATE = 오늘날짜
+		// 사원 이름 직종 직업번호
 
-		// 2. driver를 메모리에 로딩
-
-		try {
-			Class.forName(driver);
-			System.out.println("드라이버 로딩 성공");
-
-			// 3. DB서비스에 접속해서 로그인해서 Connection 받기
-			con = DriverManager.getConnection(url, user, password);
-			System.out.println("연결성공");
-
-			// 4. Query(SQL)문 생성 - 문자열 // ; 생략
-			String sql = "insert into dept values(17, 'STRIMING', 'GONGJU')";
-
-			// 5. 전송
-			st = con.createStatement();
-			result = st.executeUpdate(sql);
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-
-		} finally {
-			try {
-				st.close();
-				con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		if (result > 0) {
-			System.out.println("성공");
-		} else {
-			System.out.println("실패");
-		}
 	}
+
 }
