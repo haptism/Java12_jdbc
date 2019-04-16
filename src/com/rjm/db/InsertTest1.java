24b15bf4ca87df85575e4f1ed08acfdf231c56c5
{haptism} <haptism007@gmail.com>
2019-04-16 11:53:23 +0900

    Initial commit

---
+++ src/com/rjm/db/InsertTest1.java
@@ -0,0 +1,59 @@
+package com.rjm.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertTest1 {
+
+	public static void main(String[] args) {
+
+		// 1. DB연결 로그인
+
+		String user = "scott";
+		String password = "tiger";
+		String url = "jdbc:oracle:thin:@211.238.142.26:1521:xe"; // SID
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		Connection con = null;
+		Statement st = null;
+		int result = 0;
+
+		// 2. driver를 메모리에 로딩
+
+		try {
+			Class.forName(driver);
+			System.out.println("드라이버 로딩 성공");
+
+			// 3. DB서비스에 접속해서 로그인해서 Connection 받기
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("연결성공");
+
+			// 4. Query(SQL)문 생성 - 문자열 // ; 생략
+			String sql = "insert into dept values(11, 'OFFICE', 'BUSAN')";
+
+			// 5. 전송
+			st = con.createStatement();
+			result = st.executeUpdate(sql);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		} finally {
+			try {
+				st.close();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (result > 0) {
+			System.out.println("성공");
+		} else {
+			System.out.println("실패");
+		}
+	}
+}
---
+++ src/com/rjm/db/InsertTest4.java
@@ -0,0 +1,13 @@
+package com.rjm.db;
+
+public class InsertTest4 {
+
+	public static void main(String[] args) {
+		// EMP 테이블에 insert
+		// 사번 중복되면 에러
+		// 8가지 입력 받기 DEPTNO = 30(통일), COMM = 0, SAL = 1000, HIREDATE = 오늘날짜
+		// 사원 이름 직종 직업번호
+
+	}
+
+}
